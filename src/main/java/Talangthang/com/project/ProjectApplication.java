21d9d389a4d31f5a4c44db2d57b7ad15719cf8b2
LearningCoding101 <hahoang33322@gmail.com>
2023-05-25 14:33:16 +0700

    landing page finished except trending and feedback tab

---
+++ src/main/java/Talangthang/com/project/ProjectApplication.java
@@ -0,0 +1,21 @@
+package Talangthang.com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+	@RequestMapping("/")
+	public Resource index(){
+		return new ClassPathResource("static/index.html");
+	}
+}

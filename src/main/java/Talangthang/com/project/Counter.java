532d913582a94b96cea17fae0257c9dc5b5f0d13
LearningCoding101 <hahoang33322@gmail.com>
2023-06-05 08:56:21 +0700

    "Added user access counter"

---
+++ src/main/java/Talangthang/com/project/Counter.java
@@ -0,0 +1,63 @@
+package Talangthang.com.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+public class Counter {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    void Counter(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @GetMapping("/Count")
+    public String getCounter(){
+        String sql ="Select * from Access where Access = ?" ;
+        List<Acc> Count = jdbcTemplate.query(sql,new Object[]{"tm"}, new RowMapper<Acc>(){
+            ResultSet rs = null;
+            public Acc mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Acc test = new Acc();
+                test.setTime(rs.getInt("Time"));
+                test.setAccess(rs.getString("Access"));
+                // ...
+
+                return test;
+            }
+        });
+
+        this.jdbcTemplate.update(
+                "update Access set Time = ? where Access = ?",
+                String.valueOf(Count.get(0).getTime()+1), "tm");
+        return String.valueOf(Count.get(0).getTime());
+    }
+    private class Acc{
+        private String Access;
+        private Integer Time;
+
+        public String getAccess() {
+            return Access;
+        }
+
+        public void setAccess(String access) {
+            Access = access;
+        }
+
+        public Integer getTime() {
+            return Time;
+        }
+
+        public void setTime(Integer time) {
+            Time = time;
+        }
+    }
+}
--- src/main/java/Talangthang/com/project/ProjectApplication.java
+++ src/main/java/Talangthang/com/project/ProjectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-	@RequestMapping("/")
+	@GetMapping("/")
 	public Resource index(){
 		return new ClassPathResource("static/index.html");
 	}

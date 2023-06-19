267e3caf6347ae6b87326657e7c98962cf612ba4
LearningCoding101 <hahoang33322@gmail.com>
2023-06-19 08:30:18 +0700

    "Testing destination"

--- src/main/java/Talangthang/com/project/PlaceTypeController.java
+++ src/main/java/Talangthang/com/project/PlaceTypeController.java
@@ -24,15 +24,75 @@ class PlaceTypeController {
     @GetMapping("/DestinationResources")
     public List<Map<String, Object>> getDes(){
         /*String sql = "Select Destination.*, Picture.* FROM Destination Join Picture Where Destination.DesID = Picture.DestinationID ";*/
-        String sql = "SELECT Destination.WardID, Destination.DesID, Destination.TypeID, Destination.DesName, " +
-                "Ward.WardID, Ward.DistrictID, " +
-                "District.ProvinceID, District.DistrictID, " +
-                "Province.ProvinceID, Province.ProvinceName, Picture.* " +
-                "FROM Destination " +
-                "JOIN Picture ON Destination.DesID = Picture.DestinationID " +
-                "JOIN Ward ON Destination.WardID = Ward.WardID " +
-                "JOIN District ON Ward.DistrictID = District.DistrictID " +
-                "JOIN Province ON District.ProvinceID = Province.ProvinceID";
+        String sql ="SELECT " +
+                "Destination.WardID, " +
+                "Destination.DesID, " +
+                "Destination.TypeID, " +
+                "Destination.DesName, " +
+                "Ward.WardID, " +
+                "Ward.DistrictID, " +
+                "District.ProvinceID, " +
+                "District.DistrictID, " +
+                "Province.ProvinceID, " +
+                "Province.ProvinceName, " +
+                "Picture.* " +
+                "FROM " +
+                "Destination " +
+                "JOIN " +
+                "Picture ON Destination.DesID = Picture.DestinationID " +
+                "JOIN " +
+                "Ward ON Destination.WardID = Ward.WardID " +
+                "JOIN " +
+                "District ON Ward.DistrictID = District.DistrictID " +
+                "JOIN " +
+                "Province ON District.ProvinceID = Province.ProvinceID " +
+                "UNION " +
+                "SELECT " +
+                "Destination.WardID, " +
+                "Destination.DesID, " +
+                "Destination.TypeID, " +
+                "Destination.DesName, " +
+                "Ward.WardID, " +
+                "Ward.DistrictID, " +
+                "District.ProvinceID, " +
+                "District.DistrictID, " +
+                "Province.ProvinceID, " +
+                "Province.ProvinceName, " +
+                "Picture.* " +
+                "FROM " +
+                "Destination " +
+                "JOIN " +
+                "Ward ON Destination.WardID = Ward.WardID " +
+                "JOIN " +
+                "District ON Ward.DistrictID = District.DistrictID " +
+                "JOIN " +
+                "Province ON District.ProvinceID = Province.ProvinceID " +
+                "JOIN " +
+                "Picture ON Ward.WardID = Picture.DestinationID " +
+                "UNION " +
+                "SELECT " +
+                "Destination.WardID, " +
+                "Destination.DesID, " +
+                "Destination.TypeID, " +
+                "Destination.DesName, " +
+                "Ward.WardID, " +
+                "Ward.DistrictID, " +
+                "District.ProvinceID, " +
+                "District.DistrictID, " +
+                "Province.ProvinceID, " +
+                "Province.ProvinceName, " +
+                "Picture.* " +
+                "FROM " +
+                "Destination " +
+                "JOIN " +
+                "Ward ON Destination.WardID = Ward.WardID " +
+                "JOIN " +
+                "District ON Ward.DistrictID = District.DistrictID " +
+                "JOIN " +
+                "Province ON District.ProvinceID = Province.ProvinceID " +
+                "JOIN " +
+                "Picture ON Province.ProvinceID = Picture.DestinationID;"
+        ;
         return jdbcTemplate.queryForList(sql);
     }
     @GetMapping("/ProvinceResource")

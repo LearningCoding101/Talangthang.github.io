6de30650880b10dd9c5628329317eb43927b5576
LearningCoding101 <hahoang33322@gmail.com>
2023-06-22 13:23:56 +0700

    "updating Destination.html"

--- src/main/java/Talangthang/com/project/ProjectApplication.java
+++ src/main/java/Talangthang/com/project/ProjectApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @RestController
@@ -47,8 +55,27 @@ public class ProjectApplication {
 	public Resource About(){
 		return new ClassPathResource("static/about.html");
 	}
-	@GetMapping(value = {"/Information/{name}"})
-	public Resource getDefault(@PathVariable(required = false) String name) {
-		return new ClassPathResource("static/Default.html");
+	@RequestMapping(value = {"/Information/{name}","/Information"})
+	public ResponseEntity<String> getDefault(@PathVariable(required = false) String name) {
+		String htmlContent;
+
+		try {
+			ClassPathResource resource = new ClassPathResource("static/Default.html");
+			byte[] fileBytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+			htmlContent = new String(fileBytes, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			// Handle exception if the file cannot be read
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_HTML);
+
+		return new ResponseEntity<>(htmlContent, headers, HttpStatus.OK);
 	}
+	@GetMapping("/getCSS")
+	public Resource getCSS(){
+		return new ClassPathResource("static/style.css");
+	}
+
 }
